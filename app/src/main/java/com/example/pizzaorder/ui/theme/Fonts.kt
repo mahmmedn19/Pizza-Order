3b1af20762650edec2ac2f5753fabf406c180715
Mohamed Naser <mahmmedn19@gmail.com>
2023-07-10 14:34:57 +0300

    add image IngredientList and icons

---
+++ app/src/main/java/com/example/pizzaorder/ui/theme/Fonts.kt
@@ -0,0 +1,18 @@
+/*
+ * Created by Mohamed Naser on 7/10/23, 10:34 AM, 2023.
+ *
+ * Copyright (c) 2023 All rights reserved.
+ */
+
+package com.example.pizzaorder.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.pizzaorder.R
+
+val Poppins = FontFamily(
+    Font(R.font.poppins_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.poppins_medium, weight = FontWeight.Medium),
+    Font(R.font.poppins, weight = FontWeight.Normal)
+)

435148a4149190488357e46c0d041e3cd07d03de
Mohamed Naser <mahmmedn19@gmail.com>
2023-07-10 10:35:11 +0300

    add necessary pkg and files

---
+++ app/src/main/java/com/example/pizzaorder/screens/Pizza.kt
@@ -0,0 +1,10 @@
+/*
+ * Created by Mohamed Naser on 7/10/23, 9:31 AM, 2023.
+ *
+ * Copyright (c) 2023 All rights reserved.
+ *
+ * Last modified: 7/10/23, 9:31 AM
+ */
+
+package com.example.pizzaorder.screens
+
